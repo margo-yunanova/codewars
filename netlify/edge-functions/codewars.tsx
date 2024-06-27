41fc7041a1dfd43c8a2df8b2445b421adfacb575
Margo <m.yunanova@gmail.com>
2024-06-27 16:33:34 +0300

    fix: update locale number format

--- netlify/edge-functions/codewars.tsx
+++ netlify/edge-functions/codewars.tsx
@@ -1,64 +1,47 @@
 import type { Config, Context } from "@netlify/edge-functions";
 import { getUser } from "../../src/api.ts";
+import { simpleStats } from "../../src/stats.ts";
 
 const TOTAL_USER = 646000;
-const svg = `<svg width="800" height="600" xmlns="http://www.w3.org/2000/svg">
-  <g>
-    <title>Layer 1</title>
-    <rect rx="5" id="svg_1" height="170" width="373.99999" y="12" x="9" fill="#000000" stroke="#000" />
-    <path id="svg_5"
-      d="m230.41507,115l29.35716,-36.5l78.28575,0l29.35715,36.5l-29.35715,36.5l-78.28575,0l-29.35716,-36.5z"
-      stroke-width="5" stroke="#a855f7" fill="none" />
-    <text transform="matrix(1.72157 0 0 1.38001 -282.47 -74.5433)" stroke="#ffffff" font-style="normal"
-      font-weight="normal" xml:space="preserve" text-anchor="start" font-family="'Assistant'" font-size="18"
-      stroke-width="0" id="svg_3" y="143.34815" x="317.52163" fill="#a855f7">RANK</text>
-    <text transform="matrix(1.72157 0 0 1.38001 -282.47 -74.5433)" stroke="#ffffff" font-style="normal"
-      font-weight="normal" xml:space="preserve" text-anchor="start" font-family="'Assistant'" font-size="18"
-      stroke-width="0" id="svg_4" y="93.70092" x="179.30748" fill="#a855f7">Codewars Stats</text>
-    <text style="cursor: move;" transform="matrix(1.21312 0 0 1.38001 -133.727 -74.5433)" stroke="#ffffff"
-      font-style="normal" font-weight="normal" xml:space="preserve" text-anchor="start" font-family="'Assistant'"
-      font-size="14" stroke-width="0" id="svg_6" y="151.97168" x="245.02805" fill="#ffffff">SOLVED</text>
-    <text transform="matrix(1.21312 0 0 1.38001 -133.727 -74.5433)" stroke="#ffffff" font-style="normal"
-      font-weight="normal" xml:space="preserve" text-anchor="start" font-family="'Assistant'" font-size="14"
-      stroke-width="0" id="svg_7" y="121.60918" x="133.54445" fill="#ffffff">Honor Percentile:</text>
-    <text transform="matrix(1.21312 0 0 1.38001 -133.727 -74.5433)" stroke="#ffffff" font-style="normal"
-      font-weight="normal" xml:space="preserve" text-anchor="start" font-family="'Assistant'" font-size="14"
-      stroke-width="0" id="svg_8" y="136.97168" x="133.3656" fill="#ffffff">Honor:</text>
-    <text style="cursor: move;" transform="matrix(1.21312 0 0 1.38001 -133.727 -74.5433)" stroke="#ffffff"
-      font-style="normal" font-weight="normal" xml:space="preserve" text-anchor="start" font-family="'Assistant'"
-      font-size="14" stroke-width="0" id="svg_9" y="151.97168" x="133.54437" fill="#ffffff">Solved Katas:</text>
-    <text style="cursor: move;" transform="matrix(1.21312 0 0 1.38001 -133.727 -74.5433)" stroke="#ffffff"
-      font-style="normal" font-weight="normal" xml:space="preserve" text-anchor="start" font-family="'Assistant'"
-      font-size="14" stroke-width="0" id="svg_10" y="167.26103" x="133.3656" fill="#ffffff">Skills:</text>
-    <text style="cursor: move;" transform="matrix(1.21312 0 0 1.38001 -133.727 -74.5433)" stroke="#ffffff"
-      font-style="normal" font-weight="normal" xml:space="preserve" text-anchor="start" font-family="'Assistant'"
-      font-size="14" stroke-width="0" id="svg_11" y="166.53639" x="244.64869" fill="#ffffff">SKILLS</text>
-    <text transform="matrix(1.21312 0 0 1.38001 -133.727 -74.5433)" stroke="#ffffff" font-style="normal"
-      font-weight="normal" xml:space="preserve" text-anchor="start" font-family="'Assistant'" font-size="14"
-      stroke-width="0" id="svg_12" y="121.97168" x="245.0277" fill="#ffffff">PERCENTILE</text>
-    <text transform="matrix(1.21312 0 0 1.38001 -133.727 -74.5433)" stroke="#ffffff" font-style="normal"
-      font-weight="normal" xml:space="preserve" text-anchor="start" font-family="'Assistant'" font-size="14"
-      stroke-width="0" id="svg_13" y="136.97168" x="245.0277" fill="#ffffff">HONOR</text>
-  </g>
-</svg>`;
 
 export default async function handler(req: Request, context: Context) {
   const login = new URL(req.url).searchParams.get("login") ?? "";
 
   const user = await getUser(login);
 
-  const percentile = ((user.leaderboardPosition / TOTAL_USER) * 100).toFixed(2);
-  const skills = Object.entries(user.ranks.languages).map(([lang]) => lang);
-  const newSvg = svg
-    .replace("RANK", user.ranks.overall.name)
-    .replace("SOLVED", user.codeChallenges.totalCompleted.toString())
-    .replace("SKILLS", skills.join(", "))
-    .replace("PERCENTILE", percentile.toString() + "%")
-    .replace("HONOR", user.honor.toString());
+  const formattedPosition = new Intl.NumberFormat("ru-RU").format(
+    user.leaderboardPosition
+  );
+  const formattedPercentile = new Intl.NumberFormat("ru-RU", {
+    style: "percent",
+    maximumFractionDigits: 2,
+  }).format(user.leaderboardPosition / TOTAL_USER);
+  const formattedHonor = new Intl.NumberFormat("ru-RU").format(user.honor);
+  const formattedSolved = new Intl.NumberFormat("ru-RU").format(
+    user.codeChallenges.totalCompleted
+  );
 
-  return new Response(newSvg, {
+  const skills = Object.entries(user.ranks.languages)
+    .map(([lang, { rank }]) => [lang, rank])
+    .sort(([, a], [, b]) => {
+      if (a > b) return -1;
+      if (a < b) return 1;
+      return 0;
+    })
+    .slice(0, 3)
+    .map(([lang]) => lang);
+
+  const newsimpleStats = simpleStats
+    .replace("RANK", user.ranks.overall.name)
+    .replace("SOLVED", formattedSolved)
+    .replace("SKILLS", skills.join(", "))
+    .replace("PERCENTILE", formattedPercentile)
+    .replace("HONOR", formattedHonor)
+    .replace("POSITION", formattedPosition);
+
+  return new Response(newsimpleStats, {
     status: 200,
-    headers: { "Content-Type": "text/html" },
+    headers: { "Content-Type": "image/svg+xml" },
   });
 }
 
---
+++ src/stats.ts
@@ -0,0 +1,46 @@
+export const simpleStats = `<svg width="800" height="600" xmlns="http://www.w3.org/2000/svg">
+  <g>
+    <title>Layer 1</title>
+    <rect stroke="#000" rx="5" id="svg_1" height="198" width="401.99997" y="8" x="8" fill="#000000" />
+    <path stroke="#a855f7" id="svg_5"
+      d="m251.41507,113l29.35716,-35.5l78.28575,0l29.35715,35.5l-29.35715,35.5l-78.28575,0l-29.35716,-35.5z"
+      stroke-width="5" fill="none" />
+    <text transform="matrix(1.72157 0 0 1.38001 -282.47 -74.5433)" stroke="#ffffff" font-style="normal"
+      font-weight="normal" xml:space="preserve" text-anchor="start" font-family="'Assistant'" font-size="18"
+      stroke-width="0" id="svg_3" y="141.89889" x="328.55807" fill="#a855f7">3 kyu</text>
+    <text style="cursor: move;" transform="matrix(1.72157 0 0 1.38001 -282.47 -74.5433)" stroke="#ffffff"
+      font-style="normal" font-weight="normal" xml:space="preserve" text-anchor="start" font-family="'Assistant'"
+      font-size="18" stroke-width="0" id="svg_4" y="90.80239" x="178.72661" fill="#a855f7">Margo's Codewars Stats</text>
+    <text style="cursor: move;" transform="matrix(1.21312 0 0 1.42383 -135.727 -81.1336)" stroke="#ffffff"
+      font-style="normal" font-weight="normal" xml:space="preserve" text-anchor="start" font-family="'Assistant'"
+      font-size="14" stroke-width="0" id="svg_6" y="179.19068" x="267.82432" fill="#ffffff">SKILLS</text>
+    <text transform="matrix(1.21312 0 0 1.38001 -133.727 -74.5433)" stroke="#ffffff" font-style="normal"
+      font-weight="normal" xml:space="preserve" text-anchor="start" font-family="'Assistant'" font-size="14"
+      stroke-width="0" id="svg_7" y="118.71065" x="132.72013" fill="#ffffff">Honor Percentile:</text>
+    <text transform="matrix(1.21312 0 0 1.38001 -133.727 -74.5433)" stroke="#ffffff" font-style="normal"
+      font-weight="normal" xml:space="preserve" text-anchor="start" font-family="'Assistant'" font-size="14"
+      stroke-width="0" id="svg_8" y="134.07315" x="132.54128" fill="#ffffff">Honor:</text>
+    <text transform="matrix(1.21312 0 0 1.38001 -133.727 -74.5433)" stroke="#ffffff" font-style="normal"
+      font-weight="normal" xml:space="preserve" text-anchor="start" font-family="'Assistant'" font-size="14"
+      stroke-width="0" id="svg_9" y="164.10147" x="133.17568" fill="#ffffff">Leaderboard Position:</text>
+    <text transform="matrix(1.21312 0 0 1.38001 -133.727 -74.5433)" stroke="#ffffff" font-style="normal"
+      font-weight="normal" xml:space="preserve" text-anchor="start" font-family="'Assistant'" font-size="14"
+      stroke-width="0" id="svg_10" y="179.10147" x="133.17568" fill="#ffffff">Skills:</text>
+    <text transform="matrix(1.21312 0 0 1.38001 -133.727 -74.5433)" stroke="#ffffff" font-style="normal"
+      font-weight="normal" xml:space="preserve" text-anchor="start" font-family="'Assistant'" font-size="14"
+      stroke-width="0" id="svg_12" y="119.07315" x="266.17568" fill="#ffffff">PERCENTILE</text>
+    <text style="cursor: move;" transform="matrix(1.21312 0 0 1.38001 -133.727 -74.5433)" stroke="#ffffff"
+      font-style="normal" font-weight="normal" xml:space="preserve" text-anchor="start" font-family="'Assistant'"
+      font-size="14" stroke-width="0" id="svg_13" y="134.07315" x="266.17568" fill="#ffffff">HONOR</text>
+    <text style="cursor: move;" transform="matrix(1.21312 0 0 1.33619 -133.727 -67.9613)" stroke="#ffffff"
+      font-style="normal" font-weight="normal" xml:space="preserve" text-anchor="start" font-family="'Assistant'"
+      font-size="14" stroke-width="0" id="svg_2" y="164.00641" x="266.43234" fill="#ffffff">POSITION</text>
+    <text transform="matrix(1.21312 0 0 1.38001 -133.727 -74.5433)" stroke="#ffffff" font-style="normal"
+      font-weight="normal" xml:space="preserve" text-anchor="start" font-family="'Assistant'" font-size="14"
+      stroke-width="0" id="svg_11" y="149.07315" x="132.72005" fill="#ffffff">Solved Katas:</text>
+    <text transform="matrix(1.21312 0 0 1.38001 -133.727 -74.5433)" stroke="#ffffff" font-style="normal"
+      font-weight="normal" xml:space="preserve" text-anchor="start" font-family="'Assistant'" font-size="14"
+      stroke-width="0" id="svg_14" y="148.34852" x="266.17568" fill="#ffffff">SOLVED</text>
+  </g>
+
+</svg>`;
